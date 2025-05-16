d08a575ca15430f6c5d254f5d35cb475778ec66a
Ben S <bensn2468@gmail.com>
2025-05-15 20:07:04 -0400

    drivetrain binds and stuff - this file did not build on my computer, I am unsure if it is a me problem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,6 +3,7 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Elevator;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -14,10 +15,13 @@ public class RobotContainer {
   Shooter shooter;
   Elevator elevator;
   RobotConfig robotConfig;
+  Drivetrain drivetrain;
 
   CommandXboxController operatorController;
   CommandXboxController driveController;
   
+  SwerveRequest.FieldCentric drive;
+  SwerveRequest.FieldCentricFacingAngle reefLock;   
 
   public RobotContainer() {
     driveController = new CommandXboxController(robotConfig.driverControllerPort);
@@ -29,11 +33,55 @@ public class RobotContainer {
     climber = new Climber(robotConfig.climberConfig);
     elevator = new Elevator(robotConfig.elevatorConfig);
     shooter = new Shooter(robotConfig.shooterConfig);
+    drivetrain = new Drivetrain(robotConfig.drivetrainConfig);
+
     configureBindings();
   }
 
 
   private void configureBindings() {
+
+    //Drivetrain Bindings
+ driveController
+        .back()
+        .debounce(.25)
+        .and(driveController.start().negate())
+        .onTrue(cameraCalibrator.runCameraCalibration());
+    driveController
+        .start()
+        .debounce(.25)
+        .and(driveController.back().negate())
+        .onTrue(drivetrain.resetPerspective());
+
+    driveController
+        .rightTrigger(.5)
+        .whileTrue(drivetrain.applyRequest(
+            () -> drive
+                .withVelocityX(Drivetrain.MAX_LINEAR_SPEED.times(-controllers.getDriverLeftY()
+                    * 0.125)) // Drive forward with negative Y (forward)
+                .withVelocityY(Drivetrain.MAX_LINEAR_SPEED.times(
+                    -controllers.getDriverLeftX() * 0.125)) // Drive left with negative X (left)
+                .withRotationalRate(drivetrain
+                    .getMaxAngularVelocity()
+                    .times(-controllers.getDriverRightX()
+                        * 0.125)) // Drive counterclockwise with negative X (left)
+            ));
+
+    driveController
+        .a()
+        .and(driveController
+            .rightBumper()
+            .negate()
+            .and(driveController.leftBumper().negate()))
+        .whileTrue(drivetrain.applyRequest(() -> reefLock
+            .withVelocityX(Drivetrain.MAX_LINEAR_SPEED.times(
+                -controllers.getDriverLeftY())) // Drive forward with negative Y (forward)
+            .withVelocityY(Drivetrain.MAX_LINEAR_SPEED.times(
+                -controllers.getDriverLeftX())) // Drive left with negative X (left)
+            .withTargetDirection(fieldMap.getLockedReefFaceAngle(
+                fieldMap.getLockedReefFace(drivetrain.getPose())))));
+
+
     if (robotConfig.hasClimber) {
       operatorController
           .leftTrigger(0.5)
@@ -107,7 +155,42 @@ public class RobotContainer {
       }
   }
 
+  private void initDrivetrain(Drivetrain.Config config) {
+    drivetrain = Drivetrain.makeDrivetrain(config);
+
+    if (drivetrain != null) {
+      drive = new SwerveRequest.FieldCentric()
+          .withDriveRequestType(
+              DriveRequestType.Velocity); // Use open-loop control for drive motors
+
+      reefLock = new FieldCentricFacingAngle()
+          .withDeadband(Drivetrain.MAX_LINEAR_SPEED.times(0.1))
+          .withDriveRequestType(DriveRequestType.Velocity);
+      reefLock.HeadingController.setP(4);
+      reefLock.HeadingController.setI(0);
+      reefLock.HeadingController.setD(0);
+
+      drivetrain.setDefaultCommand(
+          // Drivetrain will execute this command periodically
+          drivetrain.applyRequest(
+              () -> drive
+                  .withVelocityX(Drivetrain.MAX_LINEAR_SPEED.times(
+                      -controllers.getDriverLeftY())) // Drive forward with negative Y (forward)
+                  .withVelocityY(Drivetrain.MAX_LINEAR_SPEED.times(
+                      -controllers.getDriverLeftX())) // Drive left with negative X (left)
+                  .withRotationalRate(drivetrain
+                      .getMaxAngularVelocity()
+                      .times(
+                          -controllers
+                              .getDriverRightX())) // Drive counterclockwise with negative X (left)
+              ));
+
+      configureDriverBindings(config);
+      registerDriverNamedCommands(config);
+    }
+  }
   public Command getAutonomousCommand() {
     return null;
   }
+    
 }
