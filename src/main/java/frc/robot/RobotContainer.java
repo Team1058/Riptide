7e876d6572e3258cd37eb36db587a31981f9c267
Surface Laptop 2 <mentors@team1058.com>
2025-06-06 20:57:02 -0400

    Rip out auto zero

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -113,10 +113,6 @@ public class RobotContainer {
         operatorController.a()
         .whileTrue(elevator.setRequestedPositionCommand(elevator.LEVELHP)
         .andThen(elevator.goToRequestedPositionCommand())
-        .until(elevator.isAtPositionTrigger(elevator.LEVELHP))
-        .andThen(elevator.resetElevatorCommand())
-        .andThen(elevator.setRequestedPositionCommand(elevator.LEVELHP))
-        .andThen(elevator.goToRequestedPositionCommand())
         .andThen(shooter.intakeCoralCommand()));
         
         // Going to Coral positions commands
