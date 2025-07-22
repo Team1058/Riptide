201f0c24137404074def622b63a0f591e9390786
Surface Laptop 1 <mentors@example.com>
2025-07-22 18:16:57 -0400

    WIP tunable PID

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -111,6 +111,7 @@ public class Robot extends LoggedRobot {
   public void testInit() {
     // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
+    robotContainer.elevator.testInit();
   }
 
   /** This function is called periodically during test mode. */
